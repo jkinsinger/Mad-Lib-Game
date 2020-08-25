52003b1303cd8ed30bacaeaa3edc4ae822d64e89
Joy Kinsinger <jhpkinsinger@Joys-MacBook-Air.local>
2020-08-25 18:16:32 -0500

    started coding Thanksgiving MadLib story

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+
 function getStory() {
-    var noun = document.getElementById('noun').value;
+    var noun = document.getElementById('noun1').value;
+    var personinRoom = document.getElementById(personinRoom).value;
     var verb = document.getElementById('verb').value;
-    var adjective = document.getElementById('adjective').value;
+    var partOfBodyPlural1 = document.getElementById('partOfBodyPlural1').value;
+    var adjective1 = document.getElementById("adjective1").value;
+    var noun2 = document.getElementById('noun2').value;
+    var noun3 = document.getElementById('noun3').value;
+    var pluralNoun = document.getElementById('pluralNoun').value;
+    var typeOfLiquid = document.getElementById('typeOfLiquid').value;
+    var adjective2 = document.getElementById('adjective2').value;
+    var noun4 = document.getElementById('noun4').value;
+    var noun5 = document.getElementById('noun5').value;
+    var pluralNoun2 = document.getElementById('pluralNoun2').value;
+    var personinRoomFemale = document.getElementById('personInRoomFemale').value;
+    var noun6 = document.getElementById('noun6').value;
+    var partOfBodyPlural2 = document.getElementById('partOfBodyPlural2').value;
     var adverb = document.getElementById('adverb').value
 
-    document.getElementById('postNoun').innerHTML = noun;
-    document.getElementById('postVerb').innerHTML = verb;
-    document.getElementById('postAdjective').innerHTML = adjective;
-    document.getElementById('postAdverb').innerHTML = adverb;
+    window.alert ("The " + adjective + " " + noun + " " + adverb + " " + verb + " away .").
+
+    console.log(noun);
+    // console.log(postNoun);
+    console.log(getStory())
 }
