44209170f62a4aa12361491f9430aa32ada185fb
Joy Kinsinger <jhpkinsinger@Joys-MacBook-Air.local>
2020-08-27 16:38:46 -0500

    added more code for Thanksgiving MadLib

--- script.js
+++ script.js
@@ -12,13 +12,15 @@ function getStory() {
     var adjective2 = document.getElementById('adjective2').value;
     var noun4 = document.getElementById('noun4').value;
     var noun5 = document.getElementById('noun5').value;
+    var noun6 = document.getElementById('noun6').value;
     var pluralNoun2 = document.getElementById('pluralNoun2').value;
     var personinRoomFemale = document.getElementById('personInRoomFemale').value;
-    var noun6 = document.getElementById('noun6').value;
+    var noun7 = document.getElementById('noun').value;
     var partOfBodyPlural2 = document.getElementById('partOfBodyPlural2').value;
-    var adverb = document.getElementById('adverb').value
 
-    window.alert ("The " + adjective + " " + noun + " " + adverb + " " + verb + " away .").
+
+
+    window.alert ("It was Thanksgiving, and the scent of succulent roast " + noun + " wafted through my house. ' " + personinRoom + ", it is time to " + verb + "!' my mother called. I could not wait to get my " + partOfBodyPlural1 + " on that " + adjective1 + " Thanksgiving meal. My family sat around the dining-room " + noun2 + ". The table was laid out with every kind of " + noun3 + " imaginable. There was a basket of hot buttered " + pluralNoun + " and glasses of sparkling " + typeOfLiquid + ". The " + adjective2 + " turkey sat, steaming, next to a tureen of " + noun4 + "gravy. A bowl of ruby-red " + noun5 + " sauce, a sweet-" + noun6 + " casserole, and a dish of mashed " + pluralNoun2 + " tempted my taste buds. But the dish I looked forward to most was Grandma " + personinRoomFemale + "'s famous " + noun7 + " pie. Thanksgiving is my favorite holiday, " + partOfBodyPlural2 + " down." )
 
     console.log(noun);
     // console.log(postNoun);
